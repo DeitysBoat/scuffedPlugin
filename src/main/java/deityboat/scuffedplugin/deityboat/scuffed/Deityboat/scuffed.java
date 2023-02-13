a8c81ca61f07c7f2c2ee96294717e2fcc2dedb16
Deity Boat <broxxil085@gmail.com>
2023-02-13 11:56:01 -0500

    yeah i made a change

--- src/main/java/deityboat/scuffedplugin/deityboat/scuffed/Deityboat/scuffed.java
+++ src/main/java/deityboat/scuffedplugin/deityboat/scuffed/Deityboat/scuffed.java
@@ -1,14 +1,27 @@
 package deityboat.scuffedplugin.deityboat.scuffed.Deityboat;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class scuffed extends JavaPlugin {
-    // hhello this is a comment i DUCKW SUP
+public final class scuffed extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
+        getServer().getPluginManager().registerEvent(this, this);
+    }
 
+    @EventHandler
+    public void onBedLeave(PlayerBedEnterEvent e) {
+        Player player = e.getPlayer();
+
+        Bukkit.broadcastMessage(player.getName() + ChatColor.GREEN + " is sleeping...");
 
     }
+
 
     @Override
     public void onDisable() {
