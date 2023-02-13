adda57457c63a8928a331257de2fa5b20c6f7be8
Deity Boat <broxxil085@gmail.com>
2023-02-13 11:43:54 -0500

    hey lol

---
+++ src/main/java/deityboat/scuffedplugin/deityboat/scuffed/Deityboat/scuffed.java
@@ -0,0 +1,16 @@
+package deityboat.scuffedplugin.deityboat.scuffed.Deityboat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class scuffed extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
